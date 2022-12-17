6a80d861897e149af68780afe88d66d0db0ef7a5
ZolarV <ngrtype31h491@gmail.com>
2022-12-17 15:47:55 -0500

    add redirect URI for SPA  + azure msal-browser

---
+++ react-app/src/Config.js
@@ -0,0 +1,10 @@
+export const config = {
+    appId: 'a979c96b-a4f2-4050-9a7b-01ac6bac7105',
+    redirectUri: 'https://orange-sea-06bd4430f.2.azurestaticapps.net',
+    scopes: [
+        'user.read'
+
+    ],
+    authortiy: 'https://login.microsoftonline.com/c2consultants.net'
+
+}
