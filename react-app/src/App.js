74c2e1ae53eeb8d4a2baa222ab28802721de2302
ZolarV <ngrtype31h491@gmail.com>
2022-12-17 16:27:14 -0500

    update code for login

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,8 @@ import './styles.scss';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { HeaderBar, NavBar, NotFound } from './components';
+import { config } from './Config';
+import  {PublicClientApplication } from '@azure/msal-browser'
 import About from './About';
 
 const Products = withRouter(
@@ -11,6 +13,54 @@ const Products = withRouter(
 );
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isAuthenticated: false,
+      user: {}
+    };
+    this.login = this.login.bind(this)
+    this.PublicClientApplication = new PublicClientApplication({
+      auth:{
+        clientId: config.appId,
+        redirectUri: config.redirectUri,
+        authority: config.authortiy
+      },
+      cache:{
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true
+      }
+    });
+  }
+
+  async login(){
+    try{
+      await this.PublicClientApplication.loginPopup(
+        {
+          scopes:config.scopes,
+          prompt: "select_account"
+        }
+      );
+    this.setState({isAuthenticated:true})
+    }
+    catch(err){
+      this.setState({
+        isAuthenticated:false,
+        user:{},
+        error:err
+      });
+    }
+  }
+  
+  logout(){
+    this.PublicClientApplication.logout();
+  }
+  
+
+
+
   render() {
     return (
       <div>
@@ -18,14 +68,22 @@ class App extends Component {
         <div className="section columns">
           <NavBar />
           <main className="column">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Switch>
-                <Redirect from="/" exact to="/products" />
-                <Route path="/products" component={Products} />
-                <Route path="/about" component={About} />
-                <Route exact path="**" component={NotFound} />
-              </Switch>
-            </Suspense>
+            {
+              this.state.isAuthenticated ? <p>
+                <Suspense fallback={<div>Loading...</div>}>
+               <Switch>
+                 <Redirect from="/" exact to="/products" />
+                 <Route path="/products" component={Products} />
+                 <Route path="/about" component={About} />
+                 <Route exact path="**" component={NotFound} />
+               </Switch>
+             </Suspense>
+              </p>:
+               <p>
+                <button onClick={()=>this.login}>Log in</button>
+               </p>
+            }
+           
           </main>
         </div>
       </div>
