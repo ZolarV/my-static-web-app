531cd8d569eb438fe2c490694afa1c9159f91f43
ZolarV <ngrtype31h491@gmail.com>
2022-12-18 01:35:13 -0500

    updates?

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -61,79 +61,27 @@ class App extends Component {
 
   render() {
     return (
-      <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h1>Status Page</h1>
-        </div>
-      </div>
-      <div class="row clearfix">
-          <div class="col-md-12 column">
-              <div class="panel panel-warning">
-                <div class="panel-heading">
-                  <h3 class="panel-title">
-                    Not All Systems Operational
-                    <small class="pull-right">Refreshed 39 minutes ago</small>
-                  </h3>
-                </div>                
-              </div>
-            
 
-              <div class="row clearfix">
-                  <div class="col-md-12 column">
-                      <div class="list-group">
-                        
-                          <div class="list-group-item">
-                              <h4 class="list-group-item-heading">
-                                  Website and API 
-                                  <a href="#"  data-toggle="tooltip" data-placement="bottom" title="Access website and use site API">
-                                    <i class="fa fa-question-circle"></i>
-                                  </a>
-                              </h4>
-                              <p class="list-group-item-text">
-                                  <span class="label label-danger">Not Operational</span>
-                              </p>
-                          </div>
-                        
-                          <div class="list-group-item">
-                              <h4 class="list-group-item-heading">
-                                  SSH 
-                                  <a href="#"  data-toggle="tooltip" data-placement="bottom" title="Access site using SSH terminal">
-                                    <i class="fa fa-question-circle"></i>
-                                  </a>
-                              </h4>
-                              <p class="list-group-item-text">
-                                  <span class="label label-success">Operational</span>
-                              </p>
-                          </div>
-                        
-                          <div class="list-group-item">
-                              <h4 class="list-group-item-heading">
-                                  Database Server 
-                                  <a href="#"  data-toggle="tooltip" data-placement="bottom" title="Access database server and execute queries">
-                                    <i class="fa fa-question-circle"></i>
-                                  </a>
-                              </h4>
-                              <p class="list-group-item-text">
-                                  <span class="label label-success">Operational</span>
-                              </p>
-                          </div>
-                          
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+  
 
 
 
-
-      
-          
-           
-           
-          
+      <div className="App">
+          <header className="App-header">         
+            {
+              this.state.isAuthenticated ? <p>               
+                <Redirect from="/" exact to="/status" />
+              </p>:
+                <div id="parent">
+                  <form id="form_login">
+                    <p>
+                    <button onClick={() => this.login()}>Login</button>
+                    </p>
+                  </form>
+                </div>
+            }
+          </header>
+        </div>    
     );
   }
 }
